e6e3e09683d3459174dc88aef1b636f2996ffe2f
Ralf Pannemans <ralf.pannemans@googlemail.com>
2018-05-15 22:12:21 +0200

    Add initial GraphData

--- src/model/graph_data.go
+++ src/model/graph_data.go
@@ -6,3 +6,34 @@ type Edge struct {
 	destination string
 	weight      int
 }
+
+//GraphData representing a graph
+type graphData struct {
+	nodes map[string]bool
+}
+
+//NewGraphData should be used to create graphdata
+func NewGraphData() *graphData {
+	g := new(graphData)
+	g.nodes = make(map[string]bool)
+	return g
+}
+
+//Add an edge to the graph
+func (graph *graphData) Add(edge Edge) {
+	graph.nodes[edge.source] = true
+	graph.nodes[edge.destination] = true
+}
+
+//GetNodes of this graph
+func (graph graphData) GetNodes() []string {
+	keys := make([]string, len(graph.nodes))
+
+	i := 0
+	for k := range graph.nodes {
+		keys[i] = k
+		i++
+	}
+
+	return keys
+}
--- src/model/graph_data_test.go
+++ src/model/graph_data_test.go
@@ -12,3 +12,29 @@ func TestCreateEdge(t *testing.T) {
 	assert.Equal(t, "B", e.destination)
 	assert.Equal(t, 42, e.weight)
 }
+
+func TestEmptyGraph(t *testing.T) {
+	g := NewGraphData()
+
+	assert.Len(t, g.GetNodes(), 0)
+}
+
+func TestAddOneEdge(t *testing.T) {
+	g := NewGraphData()
+	g.Add(Edge{"A", "B", 1})
+
+	nodes := g.GetNodes()
+	assert.Len(t, nodes, 2)
+	assert.Contains(t, nodes, "A")
+	assert.Contains(t, nodes, "B")
+}
+
+func TestAddTwoEdgesWithCommonSource(t *testing.T) {
+	g := NewGraphData()
+	g.Add(Edge{"A", "B", 1})
+	g.Add(Edge{"A", "C", 3})
+
+	nodes := g.GetNodes()
+	assert.Len(t, nodes, 3)
+	assert.Contains(t, nodes, "C")
+}
